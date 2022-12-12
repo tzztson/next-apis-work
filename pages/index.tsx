3d160c654babb7ff6724e145e6fa8427b0f889ba
tzztson <zhenghu61919@gmail.com>
2022-12-12 21:55:40 +0800

    add: added MUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,68 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import React, { useState, useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number
+) {
+  return { name, calories, fat, carbs, protein };
+}
 
 export default function Home() {
+  const [items, setItems] = useState([]);
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+
+  const getUsers = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((res) => {
+        setItems(res);
+        setIsDataLoaded(true);
+      });
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
     <div className="p-8 flex justify-center items-center">
-      你 好, 郑 虎<span className="font-black pl-8">Hello, Hu Zheng</span>
+      {isDataLoaded ? (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Full Name</TableCell>
+                <TableCell align="right">UserName</TableCell>
+                <TableCell align="right">Email</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {items.map((item: any) => (
+                <TableRow
+                  key={item.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="item">
+                    {item.name}
+                  </TableCell>
+                  <TableCell align="right">{item.username}</TableCell>
+                  <TableCell align="right">{item.email}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <div>Loading ...</div>
+      )}
     </div>
   );
 }
